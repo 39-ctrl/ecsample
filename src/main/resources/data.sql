d02a4348c2992dfb95eeec62ca36f5b749ee44d6
Taro Tanaka <taro.tanaka@example.com>
2026-05-12 11:03:52 +0900

    ログイン・ログアウト機能を追加

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,3 +3,4 @@ INSERT INTO products (name, price) VALUES
   ('コーヒーカップ', 1200),
   ('ティーポット',   2400),
   ('マグカップ',     1500);
+  
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,4 +1,6 @@
 DROP TABLE IF EXISTS products;
+DROP TABLE IF EXISTS users;
+
 
 CREATE TABLE products (
   id    SERIAL PRIMARY KEY,
@@ -6,3 +8,9 @@ CREATE TABLE products (
   price INT          NOT NULL
 );
 
+CREATE TABLE IF NOT EXISTS users (
+  id       SERIAL PRIMARY KEY,
+  name     VARCHAR(100) NOT NULL,
+  email    VARCHAR(255) NOT NULL UNIQUE,
+  password VARCHAR(255) NOT NULL
+  );
